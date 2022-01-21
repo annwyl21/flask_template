5559267fcdcee33b3ef56f8b87ce028a1b0f91c4
annwyl21 <ellen.a.ash@gmail.com>
2022-01-21 16:59:35 +0000

    create a multi page site with templates

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+#from flask_wtf import FlaskForm
+#from wtforms import StringField, SubmitField
 import uuid
 from simple_bar_chart import create_bar_chart
 from simple_heatmap import create_simple_heatmap
@@ -9,21 +9,23 @@ from heatmap_heathrow import create_heatmap_heathrow
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "my_secret"
 
-class MyForm(FlaskForm):
-    my_textfield = StringField("TextLabel")
-    my_submit = SubmitField("SubmitName")
-
+# class MyForm(FlaskForm):
+#     my_textfield = StringField("TextLabel")
+#     my_submit = SubmitField("SubmitName")
+      
 @app.route("/", methods=["GET", "POST"])
-def hello_world():
-    flask_form = MyForm()
-    unique_identifier = uuid.uuid4()
-    print(unique_identifier)
-    create_heatmap_heathrow(unique_identifier, 1980, 1990, 1)
-    #weather_type max, min, frost, rainfall, sunshine - numbered at the moment
-    return f'''
-        <h1>Mum's Graph</h1>
-        <img src="/static/{unique_identifier}.png">
-        '''
-        
+def index():
+  return render_template("index.html")
 
-#try to add a form class to this page before you learn validation about what can be entered into the form
+@app.route("/about")
+def about():
+  return render_template("about.html")
+
+@app.route("/graph_display")
+def graph_display():
+  #unique_identifier = uuid.uuid4()
+  #create_heatmap_heathrow(unique_identifier, 1980, 1990, 1)
+  create_heatmap_heathrow(888, 1980, 1990, 1)
+  #weather_type max, min, frost, rainfall, sunshine - numbered at the moment
+  #<img src="/static/{unique_identifier}.png">
+  return render_template("graph_display.html")
